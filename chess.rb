c6a61681c3e2ca3dd92f73579b1ae4bd48068aa0
Brett Cassedy <brett.cassedy@gmail.com>
2014-04-09 14:08:54 -0700

    Add support for standard Chess coordinate system

--- chess.rb
+++ chess.rb
@@ -325,8 +325,8 @@ class Board
   end
 
   def move(start_pos, end_pos)
-    raise "Position off board" if out_of_bounds?(start_pos) ||
-                                  out_of_bounds?(end_pos)
+    # raise "Position off board" if out_of_bounds?(start_pos) ||
+    #                               out_of_bounds?(end_pos)
     piece = self[start_pos]
     raise "No piece present at start position" if piece.nil?
     raise "Not a valid move for this piece" unless piece.moves.include?(end_pos)
@@ -389,18 +389,21 @@ end
 
 class Game
   attr_reader :board
-  TURNS = {
-    :white => :black,
-    :black => :white
-  }
-  COLORS = { :white => "White",
-             :black => "Black"
+
+  COLORS = { :white => :black,
+             :black => :white
            }
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @board = Board.new
+    player1.board = @board
+    player2.board = @board
+    @turns = {
+      :white => @player1,
+      :black => @player2
+    }
   end
 
   def play
@@ -408,29 +411,68 @@ class Game
     puts "White goes first."
     until @board.checkmate?(turn) do
       begin
+        system 'clear'
+        puts "Check!" if @board.in_check?(turn)
         puts @board
-        start_pos, end_pos = get_move(turn)
-        raise "Not your piece!" if @board[start_pos].color != turn
+        start_pos, end_pos = @turns[turn].play_turn(turn)
         @board.move(start_pos, end_pos)
       rescue
         retry
       end
-      turn = TURNS[turn]
+      turn = COLORS[turn]
     end
+    puts @board
+    puts "#{COLORS[turn]} wins!"
   end
 
-  def get_move(turn)
-    puts "#{COLORS[turn]} pick a move e.x. 03,05 :"
+
+end
+
+class HumanPlayer
+  attr_accessor :board
+  ALPHA_TO_COORD = {
+    'a' => 0,
+    'b' => 1,
+    'c' => 2,
+    'd' => 3,
+    'e' => 4,
+    'f' => 5,
+    'g' => 6,
+    'h' => 7
+  }
+
+  X_TO_ROW = {
+    -1 => 7,
+    -2 => 6,
+    -3 => 5,
+    -4 => 4,
+    -5 => 3,
+    -6 => 2,
+    -7 => 1,
+    -8 => 0
+  }
+
+  def play_turn(color)
+    puts "#{color} pick a move e.x. f2,f4 :"
     start_pos, end_pos = gets.chomp.split(',')
-    start_pos = start_pos.split('').map(&:to_i)
-    end_pos = end_pos.split('').map(&:to_i)
+    start_pos = translate_coordinate(start_pos)
+    puts @board[start_pos].color
+    raise "Not your piece!" if @board[start_pos].color != color
+    end_pos = translate_coordinate(end_pos)
     [start_pos, end_pos]
+  end
+
+  def translate_coordinate(chess_pos)
+    col, row = chess_pos.split('')
+    [X_TO_ROW[row.to_i * -1], ALPHA_TO_COORD[col]]
   end
 
 
 end
 
 if __FILE__ == $PROGRAM_NAME
-  g = Game.new('a', 'b')
+  p1 = HumanPlayer.new
+  p2 = HumanPlayer.new
+  g = Game.new(p1, p2)
   g.play
 end
